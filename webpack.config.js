23cefbdd8b0548cee07991de61212155964ae0b8
keyleb-mullings-cenfotec <60416684+keyleb-mullings-cenfotec@users.noreply.github.com>
2022-03-17 18:45:40 -0600

    workshop completed

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            },
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve:{
+        extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js",".jsx", ".json"],
+    },
+    output:{
+        path: path.join(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer:{
+        static: path.join(__dirname,'public/'),
+        port:3000,
+        // publicPath: 'http://localhost:3000/dist/',
+        hot: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+};
